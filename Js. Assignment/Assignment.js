404a226bf23391db815228a444fb51d846dd5e4e
ADIJAT88 <myadijatstyle@gmail.com>
2026-06-05 17:47:18 -0700

    testing Github

---
+++ Js. Assignment/Assignment.js
@@ -0,0 +1,15 @@
+const score = 70;
+
+if (score >= 80) {
+  console.log("A -Excellent!");
+} else if (score >= 70) {
+  console.log("B - Good");
+} else if (score >= 60) {
+  console.log("C - Satisfactory");
+} else if (score >= 50)  {
+    console.log("D - Need to improvement");
+} else {
+    console.log("F - Please see your teacher");
+}
+
+
